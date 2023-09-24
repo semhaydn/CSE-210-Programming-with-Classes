997143521a278b5990787af3daec0b342a9dbfc7
semhaydn <semhaydn@gmail.com>
2023-09-24 17:49:22 +0300

    initial commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,6 +6,39 @@ class Program
     {
 
         //This is prep2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is Your Grade: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+    
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+
+        string grade;
+
+        if (x >= a )
+        {
+            grade = ("A");
+        }
+        else if (x >= b)
+        {
+            grade = ("B");
+        }
+        else if (x >= c)
+        {
+            grade = ("C");   
+        }
+        else if (x >= d)
+        {
+            grade = ("D");
+        }
+        else
+        {
+            grade = ("F");
+        }
+        Console.WriteLine($"Your grade is {grade}");
+
     }
 }
