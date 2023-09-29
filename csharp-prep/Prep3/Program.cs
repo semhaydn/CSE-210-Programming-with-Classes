386b9a8c9ecb4fb9d0af4aab291534fdf4776c1e
semhaydn <semhaydn@gmail.com>
2023-09-29 17:17:09 +0300

    Initial Commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int guess;
+        int attempts = 0;
+
+        while (true)
+        {
+            int magic_number = randomGenerator.Next(1, 100);
+
+            Console.Write("Do you want to play? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response == "no")
+            {
+                Console.WriteLine("Thank you for playing. Goodbye!");
+                break;
+            }
+            else if (response != "yes")
+            {
+                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+            }
+            else
+            {
+                Console.WriteLine("Let's play!");
+
+                do
+                {
+                    Console.Write("What is your guess? ");
+                    string y = Console.ReadLine();
+                    guess = int.Parse(y);
+
+                    if (magic_number > guess)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (magic_number < guess)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break; // Exit the game loop if the guess is correct
+                    }
+
+                    attempts++;
+                } while (true); // Infinite loop for guessing
+
+                Console.WriteLine($"It took you {attempts} attempts to guess the number.");
+            }
+        }
     }
 }
