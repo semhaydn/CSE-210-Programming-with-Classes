253d7a86ea016d3e41b95a65c702911e7f885c00
semhaydn <semhaydn@gmail.com>
2023-12-12 00:00:02 +0200

    Initial commit

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Collections.Generic;
+
+class Video
+{
+    // Attributes
+    private string title;
+    private string author;
+    private int length;
+    private List<Comment> comments;
+
+    // Constructor
+    public Video(string title, string author, int length)
+    {
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.comments = new List<Comment>();
+    }
+
+    // Methods
+    public void AddComment(string commenter, string text)
+    {
+        Comment comment = new Comment(commenter, text);
+        comments.Add(comment);
+    }
+
+    public int GetNumComments()
+    {
+        return comments.Count;
+    }
+}
+
+class Comment
+{
+    // Attributes
+    private string commenter;
+    private string text;
+
+    // Constructor
+    public Comment(string commenter, string text)
+    {
+        this.commenter = commenter;
+        this.text = text;
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create a video
+        Video myVideo = new Video("Introduction to Programming", "John Doe", 120);
+
+        // Add comments
+        myVideo.AddComment("User1", "Great video!");
+        myVideo.AddComment("User2", "I learned a lot.");
+
+        // Display video information
+        Console.WriteLine($"Video: {myVideo.GetNumComments()} comments");
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,136 @@
 using System;
+using System.Collections.Generic;
+
+class Order
+{
+    // Attributes
+    private List<Product> products;
+    private Customer customer;
+
+    // Constructor
+    public Order(Customer customer)
+    {
+        this.products = new List<Product>();
+        this.customer = customer;
+    }
+
+    // Methods
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var product in products)
+        {
+            totalCost += product.Price;
+        }
+
+        // Add shipping cost based on customer location
+        totalCost += customer.IsInUSA() ? 5 : 35;
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        // Implement packing label generation
+        return "Packing Label";
+    }
+
+    public string GetShippingLabel()
+    {
+        // Implement shipping label generation
+        return "Shipping Label";
+    }
+}
+
+class Product
+{
+    // Attributes
+    private string name;
+    private string productId;
+    private double price;
+
+    // Constructor
+    public Product(string name, string productId, double price)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+    }
+
+    // Properties
+    public double Price
+    {
+        get { return price; }
+    }
+}
+
+class Customer
+{
+    // Attributes
+    private string name;
+    private Address address;
+
+    // Constructor
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Methods
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+}
+
+class Address
+{
+    // Attributes
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    // Constructor
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    // Methods
+    public bool IsInUSA()
+    {
+        return country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create a customer
+        Address customerAddress = new Address("123 Main St", "Cityville", "CA", "USA");
+        Customer myCustomer = new Customer("John Doe", customerAddress);
+
+        // Create an order
+        Order myOrder = new Order(myCustomer);
+
+        // Add products to the order
+        Product product1 = new Product("Product A", "A001", 10.99);
+        Product product2 = new Product("Product B", "B002", 20.49);
+        myOrder.AddProduct(product1);
+        myOrder.AddProduct(product2);
+
+        // Display order information
+        Console.WriteLine($"Total Cost: ${myOrder.CalculateTotalCost()}");
     }
 }
